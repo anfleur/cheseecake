7bc373ddcf782dee40e61d4c6dbd04129ec278f4
anfleur <ana.zgermano2005@gmail.com>
2022-03-08 12:07:24 -0300

    oi

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -8,13 +8,27 @@ $(document).ready(function(){
         //Coletar o que foi escrito e seecionado em nosso formulário
         let dados = $('#form').serialize()
 
+        $('#retorno').empty()
+
         $.ajax({
             type: 'POST',
             dataType: 'JSON',
             assync: true,
             data: dados,
             url: '../modelo/function.php',
-            success: function(){
+            success: function(dados){
+            $('#retorno').append(`
+            <div class="col-12 col-sm-8 col-md-6">
+                <div class="alert-primary">
+                    <h1 class="text-center text-dark">
+                        ${dados.mensagens}
+                    </h1>
+                </div>
+                <img src="../../img/${dados.tipo}" class="img-fluid">
+            </div>
+            `)
+                
+
             }
         })
 
